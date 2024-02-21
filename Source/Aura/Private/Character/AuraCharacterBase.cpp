2e584d4be095e063d78d1bfea5ee0ee325cf1845
Luiz Henrique Murakami <54338689+luizmurakamipro@users.noreply.github.com>
2024-02-21 16:51:16 -0300

    Add Aura and Enemy characters classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,14 @@
+// Copyright Druid Mechanics
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
